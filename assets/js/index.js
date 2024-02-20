d2464abc65a6f3777a4987d6964660d128860e69
JJ1312 <yeiyeithirteen@gmail.com>
2024-02-20 18:16:39 -0300

    agrega css y comportamiento jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,19 @@
 $(() => {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+  $('#enviarCorreo').on('click', ()=>{
+    alert('El correo fue enviado correctamente...');
+  });
+
+  $('#ingredientes').on('dblclick', ()=>{
+    $('#ingredientes').css( "color", "red" );
+  });
+  $('#preparacion').on('dblclick', ()=>{
+    $('#preparacion').css( "color", "red" );
+  });
+
+  $('.card-title').on('click',()=>{
+    $('.card-text').toggle('slow');
+  });
 });
